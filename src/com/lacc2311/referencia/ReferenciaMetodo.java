49a5b2e6758c8093a263b3afec6bea4d830e28d8
LACC2311 <LA_Ce@host.docker.internal>
2021-02-19 00:35:31 -0600

    Referencia a método estático

--- src/com/lacc2311/referencia/ReferenciaMetodo.java
+++ src/com/lacc2311/referencia/ReferenciaMetodo.java
@@ -4,7 +4,25 @@ public class ReferenciaMetodo {
 
 	public static void main(String[] args) {
 		
+//		Referencia a un método estático - Class::metodoEstatico
 		
+//		Usando clase anónima
+		Trabajo trabajo = new Trabajo() {
+			
+			@Override
+			public void accion() {
+				User.referenciaAMetodoEstatico();
+			}
+		};
+		trabajo.accion();
+		
+//		Usando expresión lambda
+		Trabajo trabajoL = () -> User.referenciaAMetodoEstatico();
+		trabajoL.accion();
+		
+//		Usando referencia a método (Method Reference)
+		Trabajo trabajoMR = User::referenciaAMetodoEstatico;
+		trabajoMR.accion();
 
 	}
 
