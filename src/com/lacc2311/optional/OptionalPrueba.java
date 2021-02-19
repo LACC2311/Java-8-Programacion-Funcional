1429b51c0430fc10367efd6def8c607c00f3ac68
LACC2311 <la_cepeda@hotmail.com>
2021-02-19 01:28:05 -0600

    Optional orElse para asignar valores por defecto

--- src/com/lacc2311/optional/OptionalPrueba.java
+++ src/com/lacc2311/optional/OptionalPrueba.java
@@ -5,19 +5,35 @@ import java.util.Optional;
 public class OptionalPrueba {
 	
 	public static void main(String[] args) {
-		probarOptional(null);
+//		probarOptional(null);
+		orElseOptional(null);
+		
 	}
 	
 	public static void probarOptional(String nombre) {
-//		En caso de mandar un objeto sin inicializar el método length() lanza nullpointer exception
+		// En caso de mandar un objeto sin inicializar el método length() lanza nullpointer exception
 		System.out.println(nombre.length());
 	}
 	
 	public static void crearOptional() {
-//		Se crea un optional vacio
+		// Se crea un optional vacio
 		Optional<String> optional = Optional.empty();
-//		Se obtiene el objeto del tipo declarado dentro del operador <>
+		// Se obtiene el objeto del tipo declarado dentro del operador <>
 		optional.get();
+	}
+	
+	public static void orElseOptional(String nombre) {
+		// no lanza exepción en caso de llegar nulo
+		Optional<String> optional = Optional.ofNullable(nombre);
+		// orElse: en caso de que el objeto sea nulo toma el valor por default
+		String nombreOfNullable = optional.orElse("default");
+		System.out.println(nombreOfNullable);
+		
+		// falla en caso de ser nulo
+//		Optional<String> optional1 = Optional.of(nombre);
+//		String nombreOf = optional1.orElse("default");
+//		System.out.println(nombreOf);
+		
 	}
 
 }
