b2c692f52d3c5857a0e0e818229556d3bd862e1d
LACC2311 <LA_Ce@host.docker.internal>
2021-02-19 00:41:03 -0600

    Referencia a método de instancia

--- src/com/lacc2311/referencia/ReferenciaMetodo.java
+++ src/com/lacc2311/referencia/ReferenciaMetodo.java
@@ -4,11 +4,10 @@ public class ReferenciaMetodo {
 
 	public static void main(String[] args) {
 		
-//		Referencia a un método estático - Class::metodoEstatico
+//		Referencia a un método estático 	- 	Class::metodoEstatico
 		
 //		Usando clase anónima
 		Trabajo trabajo = new Trabajo() {
-			
 			@Override
 			public void accion() {
 				User.referenciaAMetodoEstatico();
@@ -23,6 +22,18 @@ public class ReferenciaMetodo {
 //		Usando referencia a método (Method Reference)
 		Trabajo trabajoMR = User::referenciaAMetodoEstatico;
 		trabajoMR.accion();
+		
+		
+//		Referencia a método de instancia	-	instancia::metodoInstancia
+		User user = new User("Anibal");
+		
+//		Usando expresión lambda
+		Trabajo trabajoL2 = () -> user.referenciaAMetodoDeInstancia();
+		trabajoL2.accion();
+		
+//		Usando referencia a método
+		Trabajo trabajoMR2 = user::referenciaAMetodoDeInstancia;
+		trabajoMR2.accion();
 
 	}
 
