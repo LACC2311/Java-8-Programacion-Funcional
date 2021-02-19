a8c9c1e789fbb58f450a83a9691b3fe49c58e8f0
LACC2311 <LA_Ce@host.docker.internal>
2021-02-19 00:00:53 -0600

    Anotación @FunctionalInterface

--- src/com/lacc2311/lambdas/Sumar.java
+++ src/com/lacc2311/lambdas/Sumar.java
@@ -1,5 +1,7 @@
 package com.lacc2311.lambdas;
 
+// Agregamos anotación para que la interface no permita más de un método abstracto
+@FunctionalInterface
 public interface Sumar {
 
 	int suma(int a, int b);
