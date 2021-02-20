5e6566b613f053bc5d701fafe97e50c1a707feb1
LACC2311 <la_cepeda@hotmail.com>
2021-02-20 15:14:37 -0600

    Primer ejemplo de una High Order Function

--- src/com/lacc2311/highorderfunctions/HighOrderFunctions.java
+++ src/com/lacc2311/highorderfunctions/HighOrderFunctions.java
@@ -5,9 +5,20 @@ public class HighOrderFunctions implements SumarInterfaz {
 	public static void main(String[] args) {
 		HighOrderFunctions hof = new HighOrderFunctions();
 		
-		//Función tradicional
+		//Función
 		System.out.println(hof.suma(2,3));
+		
+		//Interfaz
 		System.out.println(hof.apply(2, 4));
+		
+		//High Order Functions
+		SumarInterfaz sumarInterfaz = new SumarInterfaz() {
+			@Override
+			public int apply(int a, int b) {
+				return a + b;
+			}
+		};
+		System.out.println(hof.sumaHighOrderFun(sumarInterfaz, 2, 5));
 		
 	}
 	
@@ -19,5 +30,11 @@ public class HighOrderFunctions implements SumarInterfaz {
 	public int apply(int a, int b) {
 		return a + b;
 	}
+	
+	//Las HOF pueden recibir como parámetro una función, o retornar una función
+	public int sumaHighOrderFun(SumarInterfaz sumar, int a, int b) {
+		return sumar.apply(a, b);
+	}
 
 }
+
