6991f18a3ae7a093c092aa3a87155908126ce138
LACC2311 <la_cepeda@hotmail.com>
2021-02-20 19:34:29 -0600

    Interfaz funcional Function

--- src/com/lacc2311/highorderfunctions/HighOrderFunctions.java
+++ src/com/lacc2311/highorderfunctions/HighOrderFunctions.java
@@ -1,12 +1,14 @@
 package com.lacc2311.highorderfunctions;
 
+import java.util.function.Function;
+
 public class HighOrderFunctions implements SumarInterfaz {
 
 	public static void main(String[] args) {
 		HighOrderFunctions hof = new HighOrderFunctions();
 		
 		//Función
-		System.out.println(hof.suma(2,3));
+		System.out.println(hof.suma(2, 3));
 		
 		//Interfaz
 		System.out.println(hof.apply(2, 4));
@@ -20,9 +22,13 @@ public class HighOrderFunctions implements SumarInterfaz {
 		};
 		System.out.println(hof.sumaHighOrderFun(sumarInterfaz, 2, 5));
 		
-		//Versió mejorada con lambda
-		SumarInterfaz sumarIntLambda = (a,b) -> a + b;
-		System.out.println(hof.sumaHighOrderFun(sumarIntLambda, 2, 5));
+		//Versión mejorada con lambda
+		SumarInterfaz sumarIntLambda = (a, b) -> a + b;
+		System.out.println(hof.sumaHighOrderFun(sumarIntLambda, 2, 6));
+		
+		//Interfaz funcional Function<T t, R t>
+		Function<String, String> convertirMayusculas = e -> e.toUpperCase();
+		hof.imprimirMayusculas(convertirMayusculas, "anibal");
 		
 	}
 	
@@ -39,6 +45,10 @@ public class HighOrderFunctions implements SumarInterfaz {
 	public int sumaHighOrderFun(SumarInterfaz sumar, int a, int b) {
 		return sumar.apply(a, b);
 	}
-
+	
+	//Function<Parametro de entrada, Retorno de la función>
+	public void imprimirMayusculas(Function<String, String> function, String nombre) {
+		System.out.println(function.apply(nombre));
+	}
+	
 }
-
